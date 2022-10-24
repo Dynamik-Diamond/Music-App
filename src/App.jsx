495de356f7e35b95653e2fb7eac46f3bad481c66
stone535 <dynamikdiamond2.0@gmail.com>
2022-10-24 13:54:46 +0100

    Routes Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import SharedLayout from './component/SharedLayout';
+import Home from './pages/Home';
+import PlayList from './pages/Playlist';
+import Radio from './pages/Radio';
+import MusicVideos from './pages/MusicVideos';
+import Error from './component/Error';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/playlist" element={<PlayList />} />
+        <Route path="/Radio" element={<Radio />} />
+        <Route
+          path="/music-videos"
+          element={<MusicVideos />}
+        />
+        <Route path="/*" element={<Error />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/assets/index.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import home from './Home.svg';
+import logout from './Logout.svg';
+import playlist from './playlist.svg';
+import profile from './profile.svg';
+import radio from './radio.svg';
+import videos from './videos.svg';
+import bgPhoto from './bg-photo.png';
+import bgLines from './bg-lines.svg';
+import bgLinesMobile from './bg-lines-mobile.svg';
+import likeIcon from './stroke-1.svg';
+
+export {
+  logo,
+  home,
+  logout,
+  playlist,
+  profile,
+  radio,
+  videos,
+  bgPhoto,
+  bgLines,
+  bgLinesMobile,
+  likeIcon,
+};
